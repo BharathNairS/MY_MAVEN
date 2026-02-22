98744411049fc9985534b6fa58d1567c8c6a25f4
Bharath S Nair <nairbharaths@gmail.com>
2026-02-23 03:34:13 +0530

    COMMIT

---
+++ src/main/java/ClassConditionArray/ClassAssignment.java
@@ -0,0 +1,16 @@
+package ClassConditionArray;
+
+public class ClassAssignment {
+	
+	static String b="cucumber";//static variable
+	static String c="brinjal";
+	public static void main (String args[])
+	{
+		String a= "Orange";
+        System.out.println(b);
+		System.out.println(b);
+		System.out.println(c);
+		
+	}
+
+}
