037b1841361dfd521a5a6ea4ebfdc26d8ba4af6b
Bharath S Nair <nairbharaths@gmail.com>
2026-02-23 03:10:56 +0530

    COMMIT

---
+++ src/main/java/ClassConditionArray/ClassVariableSample.java
@@ -0,0 +1,19 @@
+package ClassConditionArray;
+
+public class ClassVariableSample {
+	
+	static String c="Brinjal"; //static variable
+	static String e="Cucumber"; //static variable
+	//string d="Rose"; //non static variable instance variable
+public static void main (String args [])
+
+{
+	String a="Orange";
+	System.out.println("a"); //local variable
+	System.out.println("c");
+	System.out.println("e");
+	//System.out.println(d);
+	
+}
+
+}
