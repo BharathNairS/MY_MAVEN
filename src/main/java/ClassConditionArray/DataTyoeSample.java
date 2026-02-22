f236043e33688d90933f0be642ce982b37ce5d95
Bharath S Nair <nairbharaths@gmail.com>
2026-02-23 02:44:26 +0530

    Commit New

---
+++ src/main/java/ClassConditionArray/DataTyoeSample.java
@@ -0,0 +1,33 @@
+package ClassConditionArray;
+
+public class DataTyoeSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		byte a = 12;
+		    System.out.println(a);
+		
+		int b = 200;
+			System.out.println(b);
+		
+		short c = 4343;
+			System.out.println(c);
+			
+		long d = 547453745;
+			System.out.println(d);
+			
+		char e = 'A';
+			System.out.println(e);
+			
+		float f = 11.18f;
+			System.out.println(f);
+			
+		double g=5445.45454d;
+		boolean h= true;
+			System.out.println(g);	
+			
+
+	}
+
+}
