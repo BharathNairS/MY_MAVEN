d3c8f45b90b451547644f951d1c2cdbb2ef046e3
Bharath S Nair <nairbharaths@gmail.com>
2026-02-24 07:04:59 +0530

    COMMIT

---
+++ src/main/java/ClassConditionArray/RuntTimeError.java
@@ -0,0 +1,15 @@
+package ClassConditionArray;
+
+public class RuntTimeError {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a =10;
+		int b =a/0;
+		
+		System.out.println(b);
+
+	}
+
+}
---
+++ src/main/java/ClassConditionArray/StudentFail.java
@@ -0,0 +1,16 @@
+package ClassConditionArray;
+
+public class StudentFail {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int mark=40;
+			if (mark > 45 ) {
+				System.out.println("Passed");
+			}else {
+				System.out.println("Failed");
+			}
+
+	}
+
+}
